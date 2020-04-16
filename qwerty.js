bc26eb23ffd62542838e920679d13b36955ab95b
pavel <pglutov@gmail.com>
2020-04-16 16:56:46 +0300

    second_commit

--- qwerty.js
+++ qwerty.js
@@ -3,7 +3,11 @@ window.addEventListener('DOMContentLoaded', function() {
     let buttons = document.querySelectorAll('.box'),
           carts = document.querySelectorAll('.cart'),
           text = document.querySelector('.title'),
-          click = document.querySelector('.smd');
+          click = document.querySelector('.smd'),
+          kosmos = document.querySelector('.jpg'),
+          click2 = document.querySelector('.smd2'),
+          kosmos2 = document.querySelector('.jpg2');
+
 
           
 
@@ -21,17 +25,29 @@ window.addEventListener('DOMContentLoaded', function() {
 
             for (let i = 0; i < buttons.length; i++) {
             buttons[i].addEventListener('mouseenter', function(){
-                 text.textContent = 'suck my dick',
-                 click.style.display = 'block';
+                 text.textContent = 'i am sucking dicks now';
             });
         }
 
         for (let i = 0; i < buttons.length; i++) {
             buttons[i].addEventListener('mouseleave', function(){
-                 text.textContent = 'what are you doing?',
-                 click.style.display = 'none';
+                 text.textContent = 'what are you doing?';
             });
         }
+
+        kosmos.addEventListener('mouseenter', function(){
+            click.style.display = 'block';
+        });
+        kosmos.addEventListener('mouseleave', function(){
+            click.style.display = 'none';
+        });
+
+        kosmos2.addEventListener('mouseenter', function(){
+            click2.style.display = 'block';
+        });
+        kosmos2.addEventListener('mouseleave', function(){
+            click2.style.display = 'none';
+        });
         
 
 });
